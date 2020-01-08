b9c7469c90aef1d476eb7f9c37449bb36396c8c6
Kristof Jozsa <kristof.jozsa@gmail.com>
2020-01-08 15:28:04 +0100

    project skeleton with camel

---
+++ src/main/java/hu/dpc/zeebeehello/ZeebeeHelloApplication.java
@@ -0,0 +1,13 @@
+package hu.dpc.zeebeehello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeebeeHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeebeeHelloApplication.class, args);
+	}
+
+}
